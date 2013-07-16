2fad5a7507bcf254d777c393a258cd5ccf7905c9
MarcoLanghoff <Marco-Langhoff@web.de>
2013-07-16 17:36:46 +0000

    read index next next try

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync(index.html);
     var string = buf.toString(buffer);
