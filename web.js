b411d8e51a2ae073d52825ce17c98461cfc572f9
MarcoLanghoff <Marco-Langhoff@web.de>
2013-07-16 17:43:03 +0000

    read index ...am i stupid..yes

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
+
+buf = new Buffer(256);
 
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
