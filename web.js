1de10d6013e70927e3704a6a12c946f35d7d0c5d
MarcoLanghoff <Marco-Langhoff@web.de>
2013-07-16 17:44:36 +0000

    read index...

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-buf = new Buffer(256);
+var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
